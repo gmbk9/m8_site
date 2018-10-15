d6ed59761f6759675956e963a3a7a7aa95e0d8ca
gmbk9 <mmdo@hotmail.co.jp>
2018-10-15 19:24:27 +0900

    a

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -25,6 +33,19 @@ function cool(){
 app.get('/uncool', function(request, response) {
   response.send(uncool());
 });
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
